9493311bf218cb2560d582079f2fc068b2b4d873
SSunduy <133727309+SSunduy@users.noreply.github.com>
2025-06-04 11:55:21 +0700

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# config.example.py — пример конфигурации. НЕ ЗАЛИВАЙ СВОЙ ТОКЕН!
-
-BOT_TOKEN = '8151727623:AAFAnjOpTHwEck7XZHA-rKna-yIutmNEdw4'  # <-- заменить на свой
-ADMIN_ID = 5086485677, 537984046  # <-- заменить на свой
