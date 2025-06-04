0aa59a6a80a126bf0ddb7c4a5847418ee37d9cdc
SSunduy <133727309+SSunduy@users.noreply.github.com>
2025-06-04 11:55:56 +0700

    Delete config (1).py

--- config (1).py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-BOT_TOKEN = '8151727623:AAFAnjOpTHwEck7XZHA-rKna-yIutmNEdw4'  
-ADMIN_ID = 5086485677, 537984046  # <-- список админов
-
-SUPABASE_URL = "https://abcde.supabase.co" 
-SUPABASE_KEY = "https://azykbrodfqkdddqtplfk.supabase.co/"
